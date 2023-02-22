d147e1642b54aa2fd20cec010ca292ff50313568
Jagabee <karab.sze@gmail.com>
2023-02-21 23:30:44 -0800

    Update main

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,7 +2,7 @@ import yaml
 
 from BingService import BingService
 from PyTerrierService import PyTerrierService
-from OpenAIService import OpenAIService
+from LLMService import LLMServiceFactory, LLMService
 
 from Util import pretty_print_source
 
@@ -18,14 +18,14 @@ if __name__ == '__main__':
 
         pyterrier_service = PyTerrierService()
         gpt_input_text_df = pyterrier_service.retrieve_search_query_in_dfindexer(search_text, text_df)
-        openai_service = OpenAIService(config)
-        prompt = openai_service.get_prompt(search_text, gpt_input_text_df)
+        llm_service = LLMServiceFactory().creaet_llm_service(config)
+        prompt = llm_service.get_prompt(search_text, gpt_input_text_df)
         print('===========Prompt:============')
         print(prompt)
         print('===========Search:============')
         print(search_text)
         print('===========Ground sources:============')
         pretty_print_source(gpt_input_text_df, config.get('openai_api').get('prompt').get('prompt_length_limit'))
-        response_text = openai_service.call_openai_api(prompt)
+        response_text = llm_service.call_api(prompt)
         print('===========Response text:============')
         print(response_text)
