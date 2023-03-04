ba4a62913a4d397918699864645b3c6015195b16
Michael Wan <michaelthwan@gmail.com>
2023-03-04 22:33:30 +0800

    heroku deployment work. Try real thing

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-from flask import Flask
+from src.website import create_app
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return "Hello World!"
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
