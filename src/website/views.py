94fbaca3579f536ff3df7334e7ef7be8cb7db5ca
Michael Wan <michaelthwan@gmail.com>
2023-03-05 00:21:15 +0800

    error handling in UI

--- src/website/views.py
+++ src/website/views.py
@@ -9,27 +9,31 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET'])
 @views.route('/index', methods=['GET'])
 def index_page():
-    # query string
-    search_text = request.values.get('q')
-    ui_overriden_config = {
-        'bing_search_subscription_key': request.values.get('bing_search_subscription_key'),
-        'openai_api_key': request.values.get('openai_api_key'),
-        'is_use_source': request.values.get('is_use_source'),
-        'llm_service_provider': request.values.get('llm_service_provider'),
-        'llm_model': request.values.get('llm_model'),
-        'semantic_search_provider': request.values.get('semantic_search_provider'),
-    }
-    logger.info(f"GET ui_overriden_config: {ui_overriden_config}")
+    error = None
+    try:
+        search_text = request.values.get('q')
+        ui_overriden_config = {
+            'bing_search_subscription_key': request.values.get('bing_search_subscription_key'),
+            'openai_api_key': request.values.get('openai_api_key'),
+            'is_use_source': request.values.get('is_use_source'),
+            'llm_service_provider': request.values.get('llm_service_provider'),
+            'llm_model': request.values.get('llm_model'),
+            'semantic_search_provider': request.values.get('semantic_search_provider'),
+        }
+        logger.info(f"GET ui_overriden_config: {ui_overriden_config}")
 
-    data_json = {'response_json': [], 'source_json': []}
-    if search_text is not None:
-        search_gpt_service = SearchGPTService(ui_overriden_config)
-        response_text, response_text_with_footnote, source_text, data_json = search_gpt_service.query_and_get_answer(search_text)
-        # response_text, response_text_with_footnote, source_text, data_json = "test", "test", "test", {'response_json': [], 'source_json': []}
+        data_json = {'response_json': [], 'source_json': []}
+        if search_text is not None:
+            search_gpt_service = SearchGPTService(ui_overriden_config)
+            response_text, response_text_with_footnote, source_text, data_json = search_gpt_service.query_and_get_answer(search_text)
+            # response_text, response_text_with_footnote, source_text, data_json = "test", "test", "test", {'response_json': [], 'source_json': []}
+    except Exception as e:
+        error = str(e)
     return render_template("index.html",
                            search_text=search_text or "Please search for something.",
                            response_json=data_json.get('response_json'),
                            source_json=data_json.get('source_json'),
+                           error=error
                            )
 
 
