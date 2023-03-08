60e17f84b5db88a8891f70c250d8952018df93cc
Lydia Pang <panglydia82@gmail.com>
2023-03-09 01:13:28 +0800

    Supported bing and llm cache

--- src/SearchGPTService.py
+++ src/SearchGPTService.py
@@ -63,20 +63,9 @@ class SearchGPTService:
         gpt_input_text_df = semantic_search_service.retrieve_result_by_search_text_from_text_df(search_text, text_df)
         gpt_input_text_df = post_process_gpt_input_text_df(gpt_input_text_df, self.config.get('openai_api').get('prompt').get('prompt_length_limit'))
 
-        llm_service_provider = self.config.get('llm_service').get('provider')
-        # check if llm result is cached and load if exists
-        if self.config.get('cache').get('is_enable_cache') and check_result_cache_exists(cache_path, search_text, llm_service_provider):
-            logger.info(f"SemanticSearchService.load_result_from_cache. search_text: {search_text}, cache_path: {cache_path}")
-            cache = load_result_from_cache(cache_path, search_text, llm_service_provider)
-            prompt, response_text = cache['prompt'], cache['response_text']
-        else:
-            llm_service = LLMServiceFactory.create_llm_service(self.config)
-            prompt = llm_service.get_prompt(search_text, gpt_input_text_df)
-            response_text = llm_service.call_api(prompt)
-
-            llm_config = self.config.get(f'{llm_service_provider}_api').copy()
-            llm_config.pop('api_key')  # delete api_key to avoid saving it to .cache
-            save_result_cache(cache_path, search_text, llm_service_provider, prompt=prompt, response_text=response_text, config=llm_config)
+        llm_service = LLMServiceFactory.create_llm_service(self.config)
+        prompt = llm_service.get_prompt(search_text, gpt_input_text_df)
+        response_text = llm_service.call_api(prompt=prompt)
 
         # check whether the number of cache exceeds the limit
         check_max_number_of_cache(cache_path, self.config.get('cache').get('max_number_of_cache'))
@@ -96,21 +85,10 @@ class SearchGPTService:
 
     def _extract_bing_text_df(self, search_text, cache_path):
         # BingSearch using search_text
-        #   check if bing search result is cached and load if exists
-        bing_text_df = None
-        if self.config['search_option']['is_enable_bing_search']:
-            if self.config.get('cache').get('is_enable_cache') and check_result_cache_exists(cache_path, search_text, 'bing_search'):
-                logger.info(f"BingService.load_result_from_cache. search_text: {search_text}, cache_path: {cache_path}")
-                cache = load_result_from_cache(cache_path, search_text, 'bing_search')
-                bing_text_df = cache['bing_text_df']
-            else:
-                bing_service = BingService(self.config)
-                website_df = bing_service.call_bing_search_api(search_text)
-                bing_text_df = bing_service.call_urls_and_extract_sentences_concurrent(website_df)
+        bing_service = BingService(self.config)
+        website_df = bing_service.call_bing_search_api(query=search_text)
+        bing_text_df = bing_service.call_urls_and_extract_sentences_concurrent(website_df=website_df)
 
-                bing_search_config = self.config.get('bing_search').copy()
-                bing_search_config.pop('subscription_key')  # delete api_key from config to avoid saving it to .cache
-                save_result_cache(cache_path, search_text, 'bing_search', bing_text_df=bing_text_df, config=bing_search_config)
         return bing_text_df
 
     def _extract_doc_text_df(self, bing_text_df):
