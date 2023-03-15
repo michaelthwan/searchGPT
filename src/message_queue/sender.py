f19e502e4b9c45a6540582781269ffb64cdbefa4
Michael Wan <michaelthwan@gmail.com>
2023-03-15 22:38:18 +0800

    complete UI loading progress

--- src/message_queue/sender.py
+++ src/message_queue/sender.py
@@ -9,11 +9,14 @@ from .message import Message
 class Sender:
     def __init__(self, sender_id: str):
         self.sender_id = sender_id
+        self.msg_list = []
         self.queue = Queue()
         self._stop_event = threading.Event()
 
-    def send_message(self, msg_type, msg):
-        search_result_step_html = render_template('search_result_step.html', search_result_step_json=[{'msg': msg}])
+    def send_message(self, msg_type, msg: str):
+        self.msg_list.append(msg)
+        search_result_step_html = render_template('search_result_step.html',
+                                                  search_result_step_json=[{'msg': msg} for msg in self.msg_list])
         self.queue.put(Message(sender_id=self.sender_id, msg_type=msg_type, msg={'msg': msg, 'html': search_result_step_html}))
 
     def stop(self):
